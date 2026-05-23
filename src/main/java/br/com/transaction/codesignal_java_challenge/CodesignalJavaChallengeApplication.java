e1a677e2470d256b18f5d74b0df137c254282520
“juliano-barbosa-silva” <fiap.juliano@gmail.com>
2026-05-22 22:53:52 -0300

    feat: setup project

---
+++ src/main/java/br/com/transaction/codesignal_java_challenge/CodesignalJavaChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.transaction.codesignal_java_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodesignalJavaChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodesignalJavaChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/transaction/codesignal_java_challenge/CodesignalJavaChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.transaction.codesignal_java_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodesignalJavaChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
